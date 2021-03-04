1d1c2e356a51bc19dc4d126effdf35196cdcf2ea
Alejandro <Alejandro@macbook-air-2.devices.brown.edu>
2021-03-04 17:52:43 -0500

    edited code for starting page

--- Card_Task.m
+++ Card_Task.m
@@ -13,44 +13,29 @@ Screen('Preference', 'SkipSyncTests', 1);
 % Call some default settings for setting up Psychtoolbox
 PsychDefaultSetup(2);
 
-% Get the screen numbers
 screens = Screen('Screens');
-
-% Select the external screen if it is present, else revert to the native
-% screen
 screenNumber = max(screens);
 
-% Define black, white and grey
-black = BlackIndex(screenNumber);
+% Define white, black, and grey using the luminance values for the display.
 white = WhiteIndex(screenNumber);
+black = BlackIndex(screenNumber);
 grey = white / 2;
 
-% Open an on screen window and color it black
 [window, windowRect] = PsychImaging('OpenWindow', screenNumber, black);
-
-% Set the blend funciton for the screen
-Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
-
-% Get the size of the on screen window in pixels
 [screenXpixels, screenYpixels] = Screen('WindowSize', window);
 
-% Get the centre coordinate of the window in pixels
-[xCenter, yCenter] = RectCenter(windowRect);
+%setting text for starting page
+Screen('TextSize', window, 50);
+Screen('TextFont', window, 'Courier');
+DrawFormattedText(window, 'Wisconsin Card Sorting Task Variation' ,...
+'center', screenYpixels * 0.5, [1 1 1]);
 
-line1 = 'Welcome To the Wisconsin Card Sorting Task Variation';
-line2 = '\n\n Josh Assi & Alejandro Torres';
-line3 = '\n\n Please read all the following directions and instructions';
-
-% Draw all the text in one go
 Screen('TextSize', window, 40);
-DrawFormattedText(window, [line1 line2 line3],...
-    'center', screenYpixels * 0.25, white);
+Screen('TextFont', window, 'Courier');
+DrawFormattedText(window, 'Please read all instructions carefully' ,...
+'center', screenYpixels * 0.6, [1 1 1]);
 
-% Flip to the screen
 Screen('Flip', window);
 
-%Press any key to terminate
 KbStrokeWait;
-
-% Clear the screen
 sca;
