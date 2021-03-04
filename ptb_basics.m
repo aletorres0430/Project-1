b3f8f07f02c37d47667f566d3d3622f8b532e4b2
legster <66693306+legster@users.noreply.github.com>
2021-03-03 20:06:39 -0500

    Added the ptb_basics.m that the tutorial suggested.

---
+++ ptb_basics.m
@@ -0,0 +1,9 @@
+% Boilerplate
+% Clear all screens and figures as well as all variables in the workspace
+sca;
+close all;
+clear all;
+% Skip some tests in case your initial ScreenTest command failed. Ignore
+% Screen('Preference', 'SkipSyncTests', 1);
+% Here we call some default settings for setting up Psychtoolbox
+PsychDefaultSetup(2);
