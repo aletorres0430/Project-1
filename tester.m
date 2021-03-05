cf54b0e56ce93a6b89f1c1d9ea2e3a81f70d67bd
legster <66693306+legster@users.noreply.github.com>
2021-03-05 16:44:14 -0500

    Distraction correctly responds when left or right is pressed

--- tester.m
+++ tester.m
@@ -22,6 +22,7 @@ screens = Screen('Screens');
 % this.
 screenNumber = max(screens);
 
+
 % Define black and white (white will be 1 and black 0).
 white = WhiteIndex(screenNumber);
 black = BlackIndex(screenNumber);
@@ -38,90 +39,112 @@ black = BlackIndex(screenNumber);
 % Enable alpha blending for anti-aliasing
 Screen('BlendFunction', window, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-% Query the frame duration
-ifi = Screen('GetFlipInterval', window);
+correct = 0;
+while ~correct
+    % Query the frame duration
+    ifi = Screen('GetFlipInterval', window);
 
-% Use the meshgrid command to create our base dot coordinates
-dim = 5;
-[x, y] = meshgrid(-dim:1:dim, -dim:1:dim);
+    % Use the meshgrid command to create our base dot coordinates
+    dim = 5;
+    [x, y] = meshgrid(-dim:1:dim, -dim:1:dim);
 
-% Scale this by the distance in pixels we want between each dot
-pixelScale = screenYpixels / (dim * 4 + 2);
-x = x .* pixelScale;
-y = y.* pixelScale;
+    % Scale this by the distance in pixels we want between each dot
+    pixelScale = screenYpixels / (dim * 4 + 2);
+    x = x .* pixelScale;
+    y = y.* pixelScale;
 
-% Calculate the number of dots
-numDots = numel(x);
+    % Calculate the number of dots
+    numDots = numel(x);
 
-% Make the matrix of positions for the dots into two vectors
-xPosVector = reshape(x, 1, numDots);
-yPosVector = reshape(y, 1, numDots);
+    % Make the matrix of positions for the dots into two vectors
+    xPosVector = reshape(x, 1, numDots);
+    yPosVector = reshape(y, 1, numDots);
 
-% We can define a center for the dot coordinates to be relaitive to. Here
-% we set the centre to be the centre of the screen
-dotCenter = [screenXpixels / 2 screenYpixels / 2];
+    % We can define a center for the dot coordinates to be relaitive to. Here
+    % we set the centre to be the centre of the screen
+    dotCenter = [screenXpixels / 2 screenYpixels / 2];
 
-% Set the color of our dot to be random
-dotColors = rand(3, numDots);
+    % Set the color of our dot to be random
+    dotColors = rand(3, numDots);
 
-% Set the size of the dots randomly between 10 and 30 pixels
-dotSizes = rand(1, numDots) .* 20 + 10;
+    % Set the size of the dots randomly between 10 and 30 pixels
+    dotSizes = rand(1, numDots) .* 20 + 10;
 
-% Our grid will oscilate with a sine wave function to the left and right
-% of the screen. These are the parameters for the sine wave
-% See: http://en.wikipedia.org/wiki/Sine_wave
-speed = screenYpixels * 0.15;
-time = 0;
+    % Our grid will oscilate with a sine wave function to the left and right
+    % of the screen. These are the parameters for the sine wave
+    % See: http://en.wikipedia.org/wiki/Sine_wave
+    speed = screenYpixels * 0.15;
+    time = 0;
 
-% Sync us and get a time stamp
-vbl = Screen('Flip', window);
-waitframes = 1;
+    % Sync us and get a time stamp
+    vbl = Screen('Flip', window);
+    waitframes = 1;
 
-% Maximum priority level
-topPriorityLevel = MaxPriority(window);
-Priority(topPriorityLevel);
+    % Maximum priority level
+    topPriorityLevel = MaxPriority(window);
+    Priority(topPriorityLevel);
 
-gridPos = 0;
+    gridPos = 0;
 
-activeKeys = [KbName('LeftArrow') KbName('RightArrow')];
-RestrictKeysForKbCheck(activeKeys);
+    activeKeys = [KbName('LeftArrow') KbName('RightArrow')];
+    RestrictKeysForKbCheck(activeKeys);
 
-% Loop the animation until a key is pressed
-while ~KbCheck
-    while gridPos <= 500
+    %50/50 of getting 1 or -1, this determines direction of dots
+    coinflip = randi(2);
+    if coinflip == 2
+        coinflip = -1;
+    end
+
+
+    % Loop the animation until a key is pressed
+    while ~KbCheck
+        while (gridPos < 500) && (gridPos > -500)
+            if ~KbCheck
+                % Position of the square on this frame
+                gridPos = speed * time * coinflip;
+
+                % Draw all of our dots to the screen in a single line of code adding
+                Screen('DrawDots', window, [xPosVector + gridPos; yPosVector],...
+                    dotSizes, dotColors, dotCenter, 4);
+
+                % Flip to the screen
+                vbl  = Screen('Flip', window, vbl + (waitframes - 0.5) * ifi);
+
+                % Increment the time
+                time = time + ifi;
+            else
+                break
+            end
+        end
         if ~KbCheck
-            % Position of the square on this frame
-            gridPos = speed * time;
-
-            % Draw all of our dots to the screen in a single line of code adding
-            % the sine oscilation to the X coordinates of the dots
-            Screen('DrawDots', window, [xPosVector + gridPos; yPosVector],...
-                dotSizes, dotColors, dotCenter, 2);
-
-            % Flip to the screen
-            vbl  = Screen('Flip', window, vbl + (waitframes - 0.5) * ifi);
-
-            % Increment the time
-            time = time + ifi;
+            time = 0;
+            gridPos = 0;
+            pause(1);
         else
             break
         end
     end
-    if ~KbCheck
-        time = 0;
-        gridPos = 0;
-        pause(1);
-    else
-        break
-    end
-end
-[keyIsDown, secs, keyCode] = KbCheck;
-if find(keyCode, 1) == 79
-    pressed_right = 1;
-else
-    pressed_right = 0;
-end
 
+    %if right is pressed, pressed_right is 1, if left was pressed,
+    %pressed_right is -1
+    [keyIsDown, secs, keyCode] = KbCheck;
+    if find(keyCode, 1) == 79
+        pressed_right = 1;
+    else
+        pressed_right = -1;
+    end
+
+    if coinflip == pressed_right
+        % display on screen: Correct
+        pause(2);
+        correct = 1;
+    else
+        %display on screen: Incorrect
+        pause(2);
+    end
+    
+    
+end
 
 % Clear the screen. "sca" is short hand for "Screen CloseAll". This clears
 % all features related to PTB. Note: we leave the variables in the
