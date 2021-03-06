a3c04ae4c5e202cdd7e2d485aa6a0757f0282d83
legster <66693306+legster@users.noreply.github.com>
2021-03-06 18:25:06 -0500

    Creates allcards matrix that holds the names of all images files in stimuli folder. Rule is randomly chosen and is not the same as the previous rule. Stores cards 1-5 image file names in card1...card5.

--- choose4random.m
+++ choose4random.m
@@ -12,7 +12,53 @@ card_shapes = randperm(4);
 
 %each card number is determined by its position in each shape, number, and
 %color range
-card1 = card_colors(1) + 4 * (card_numbers(1) - 1) + 16 * (card_shapes(1) - 1);
-card2 = card_colors(2) + 4 * (card_numbers(2) - 1) + 16 * (card_shapes(2) - 1);
-card3 = card_colors(3) + 4 * (card_numbers(3) - 1) + 16 * (card_shapes(3) - 1);
-card4 = card_colors(4) + 4 * (card_numbers(4) - 1) + 16 * (card_shapes(4) - 1);
+card1number = card_colors(1) + 4 * (card_numbers(1) - 1) + 16 * (card_shapes(1) - 1);
+card2number = card_colors(2) + 4 * (card_numbers(2) - 1) + 16 * (card_shapes(2) - 1);
+card3number = card_colors(3) + 4 * (card_numbers(3) - 1) + 16 * (card_shapes(3) - 1);
+card4number = card_colors(4) + 4 * (card_numbers(4) - 1) + 16 * (card_shapes(4) - 1);
+
+%card5 is a random one of the cards, but cannot be the same as one of the 4
+%displayed cards
+card5number = randi(64);
+while (card5number == card1number) || (card5number == card2number) || ...
+        (cardnumber == card3number) || (card5number == card4number)
+    card5number = randi(64);
+end
+
+
+
+%allcards is an array of the names of image files in stimuli folder with
+%stimuli/ added to the front
+allcards = dir('stimuli');
+allcards(1:2) = [];
+allcards = {allcards.name};
+allcards = string(allcards);
+for ii = 1:64
+    allcards(ii) = strcat('stimuli/', allcards(ii));
+end
+
+%each card file name is retrieved from the list in allcards
+card1 = allcards(card1number);
+card2 = allcards(card2number);
+card3 = allcards(card3number);
+card4 = allcards(card4number);
+card5 = allcards(card5number);
+
+
+
+%chooses new rule: 1 is shape, 2 is color, 3 is number
+newrule = randi(3);
+
+%makes sure the new rule is not the same as the old one
+while newrule == rule
+    newrule = randi(3);
+end
+rule = newrule;
+
+cardnumbers = [card1number card2number card3number card4number];
+if rule == 1
+    correctcard = find(floor(cardnumbers./16), floor(card5number/16));
+elseif rule == 2
+    correctcard = find(floor(cardnumbers./16), floor(card5number/16));
+
+
